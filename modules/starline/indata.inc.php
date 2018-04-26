2d039c42e7714293f691875e4715f05dac3f2b1c
directman66 <sannikovdi@yandex.ru>
2018-04-26 17:45:28 +0500

    Update indata.inc.php

--- modules/starline/indata.inc.php
+++ modules/starline/indata.inc.php
@@ -15,6 +15,25 @@
 	
   if (!$qry) $qry="1";
   // SEARCH RESULTS
+//select  nameid,titlename,descr,max(typename) tip, max(alias) alias, max(device_id) device_id, max(mayak_temp) mayak_temp,
+//max(status) status, max(arm) arm
+//from (
+//select nameid,titlename,descr
+//,if (tip='typename', VALUE,null) typename
+//,if (tip='alias', VALUE,null) alias
+//,if (tip='device_id', VALUE,null) device_id
+//,if (tip='mayak_temp', VALUE,null) mayak_temp    
+//,if (tip='status', VALUE,null) status        
+//,if (tip='arm', VALUE,null) arm        
+ //from   (
+//SELECT objects.ID nameid, objects.TITLE titlename, objects.DESCRIPTION descr, properties.TITLE tip, pvalues.VALUE
+//FROM `objects`, `properties`, `pvalues`
+ //WHERE objects.ID=properties.OBJECT_ID and objects.class_id = (SELECT ID FROM `classes` WHERE title='starline-online') 
+//and properties.ID=pvalues.PROPERTY_ID
+//    ) a
+//    )b 
+//    group by  nameid,titlename,descr
+
   $res=SQLSelect("SELECT `TITLE`,`DESCRIPTION` FROM `objects` WHERE `CLASS_ID` in(SELECT ID  FROM `classes` WHERE title='starline-online')");
   if ($res[0]['TITLE']) {
    //paging($res, 100, $out); // search result paging
