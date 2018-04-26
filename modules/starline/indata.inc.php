864617c4aaf28ca2ae6f0369a69a0f25d981d0cf
directman66 <sannikovdi@yandex.ru>
2018-04-27 02:33:45 +0500

    Update indata.inc.php

--- modules/starline/indata.inc.php
+++ modules/starline/indata.inc.php
@@ -15,27 +15,13 @@
 	
   if (!$qry) $qry="1";
   // SEARCH RESULTS
-//select  nameid,titlename,descr,max(typename) tip, max(alias) alias, max(device_id) device_id, max(mayak_temp) mayak_temp,
-//max(status) status, max(arm) arm
-//from (
-//select nameid,titlename,descr
-//,if (tip='typename', VALUE,null) typename
-//,if (tip='alias', VALUE,null) alias
-//,if (tip='device_id', VALUE,null) device_id
-//,if (tip='mayak_temp', VALUE,null) mayak_temp    
-//,if (tip='status', VALUE,null) status        
-//,if (tip='arm', VALUE,null) arm        
- //from   (
-//SELECT objects.ID nameid, objects.TITLE titlename, objects.DESCRIPTION descr, properties.TITLE tip, pvalues.VALUE
-//FROM `objects`, `properties`, `pvalues`
- //WHERE objects.ID=properties.OBJECT_ID and objects.class_id = (SELECT ID FROM `classes` WHERE title='starline-online') 
-//and properties.ID=pvalues.PROPERTY_ID
-//    ) a
-//    )b 
-//    group by  nameid,titlename,descr
+$res=SQLSelect("select  titlename TITLE,descr DESCRIPTION,max(typename) tip, max(ign) ign, max(status) status, max(arm) arm,max(etemp) etemp,max(ctemp) ctemp,max(mayak_temp) mayak_temp,max(device_id) device_idfrom (select titlename,descr,if (tip='typename', VALUE,null) typename,if (tip='alias', VALUE,null) alias,if (tip='device_id', VALUE,null) device_id,if (tip='etemp', VALUE,null) etemp,if (tip='ctemp', VALUE,null) ctemp,if (tip='mayak_temp', VALUE,null) mayak_temp ,if (tip='status', VALUE,null) status ,if (tip='arm', VALUE,null) arm ,if (tip='ign', VALUE,null) ign  from   (SELECT objects.TITLE titlename , objects.DESCRIPTION descr, substring(pvalues.PROPERTY_NAME, position('.' in pvalues.PROPERTY_NAME)+1) tip, pvalues.VALUE fROM `objects`,  `pvalues`WHERE  objects.class_id = (SELECT ID FROM `classes` WHERE title='starline-online') and objects.ID=pvalues.OBJECT_ID     )a    )b       group by  titlename,descr");
 
-  $res=SQLSelect("SELECT `TITLE`,`DESCRIPTION` FROM `objects` WHERE `CLASS_ID` in(SELECT ID  FROM `classes` WHERE title='starline-online')");
-  if ($res[0]['TITLE']) {
+  //$res=SQLSelect("SELECT `TITLE`,`DESCRIPTION` FROM `objects` WHERE `CLASS_ID` in(SELECT ID  FROM `classes` WHERE title='starline-online')");
+  
+$res=SQLSelect("
+	
+	if ($res[0]['TITLE']) {
    //paging($res, 100, $out); // search result paging
    $total=count($res);
    for($i=0;$i<$total;$i++) {
