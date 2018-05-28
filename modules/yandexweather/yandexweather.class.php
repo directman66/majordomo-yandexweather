b0a159bfb3571889f3dc8fefb89b329a31a400d5
directman66 <sannikovdi@yandex.ru>
2018-05-28 14:03:38 +0500

    Update yandexweather.class.php

--- modules/yandexweather/yandexweather.class.php
+++ modules/yandexweather/yandexweather.class.php
@@ -925,6 +925,11 @@ foreach ($objprops as $value){ sg($objmycity.'.'.$value,gg($fobjn.".".$value));}
 */
  function uninstall() {
   SQLExec('DROP TABLE IF EXISTS yaweather_cities');
+      SQLExec("delete from pvalues where property_id in (select id FROM properties where object_id in (select id from objects where class_id = (select id from classes where title = 'YandexWeather')))");
+      SQLExec("delete from properties where object_id in (select id from objects where class_id = (select id from classes where title = 'YandexWeather'))");
+      SQLExec("delete from objects where class_id = (select id from classes where title = 'YandexWeather')");
+      SQLExec("delete from classes where title = 'YandexWeather'");
+
   parent::uninstall();
  }
 /**
