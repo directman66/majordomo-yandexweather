f6c6bd43c2867d8335bb4e4f7db4129aae1fd6bc
directman66 <sannikovdi@yandex.ru>
2018-06-04 16:58:31 +0500

    Update saycondition.php

--- modules/yandexweather/saycondition.php
+++ modules/yandexweather/saycondition.php
@@ -13,10 +13,10 @@ $condition2eng=gg("yw_mycity.forecast_1_daycondition ");
 $condition3eng=gg("yw_mycity.forecast_2_daycondition ");    
 
   if ($lastcondition<>$conditioneng){
-//$condition=conditionrus($conditioneng);
-//$condition1=conditionrus($condition1eng);    
-//$condition2=conditionrus($condition2eng);        
-//$condition3=conditionrus($condition3eng);        
+$condition=conditionrus($conditioneng);
+$condition1=conditionrus($condition1eng);    
+$condition2=conditionrus($condition2eng);        
+$condition3=conditionrus($condition3eng);        
 sg("yw_mycity.lastcondition",$conditioneng);
 sg("yw_mycity.lastconditionrus",$condition); 
     
