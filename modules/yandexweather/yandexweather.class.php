398cb5ac1e24e7ac32105f24cd35157974dd5283
directman66 <sannikovdi@yandex.ru>
2018-06-04 15:02:12 +0500

    Update yandexweather.class.php

--- modules/yandexweather/yandexweather.class.php
+++ modules/yandexweather/yandexweather.class.php
@@ -1292,3 +1292,29 @@ $sql='SELECT id   FROM `objects`  WHERE TITLE ="'.$prop.'"';
 $rec = SQLSelect($sql); 
 return $rec[0][id];
 }
+
+
+function getconditionrus($condition){
+	if (($this->object_title=="yw_mycity") and ($this->getProperty("condition")<>"")){
+//if ($this->object_title=="yw_mycity") {
+$lastcondition=gg("yw_mycity.lastcondition");
+$conditioneng=gg("yw_mycity.condition");
+if ($lastcondition<>$conditioneng){
+$condition=$conditioneng; 
+if ($conditioneng=="overcast") {$condition="ясно";}
+if ($conditioneng=="cloudy-and-light-rain") {$condition="пасмурно и небольшой дождь";}
+if ($conditioneng=="cloudy-and-rain") {$condition="пасмурно и  дождь";}
+if ($conditioneng=="cloudy") {$condition="облачно";}
+if ($conditioneng=="overcast-and-light-rain") {$condition="моросящий дождь";}
+if ($conditioneng=="overcast-and-light-snow") {$condition="небольшой снег";}
+if ($conditioneng=="partly-cloudy-and-light-rain") {$condition="переменная облачность и небольшой дождь";}
+if ($conditioneng=="partly-cloudy-and-light-snow") {$condition="переменная облачность и небольшой снег";}
+if ($conditioneng=="partly-cloudy-and-rain") {$condition="переменная облачность с дождем";}
+if ($conditioneng=="partly-cloudy-and-snow") {$condition="переменная облачность со снегом";}
+if ($conditioneng=="partly-cloudy") {$condition="переменная облачность";}
+//sg("yw_mycity.lastcondition",$conditioneng) ;
+//sg("yw_mycity.lastconditionrus",$condition) ; 
+return $condition;
+}
+	
+	
