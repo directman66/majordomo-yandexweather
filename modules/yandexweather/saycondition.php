d11b56bafb9b137b02c69c1c8dc6560cb50d1072
directman66 <sannikovdi@yandex.ru>
2018-06-04 15:53:11 +0500

    Update saycondition.php

--- modules/yandexweather/saycondition.php
+++ modules/yandexweather/saycondition.php
@@ -6,23 +6,26 @@ $ee=$yw->config["ENABLE_EVENTS"];
 if ($ee=="1"){
 
 if (($this->object_title=="yw_mycity") and ($this->getProperty("condition")<>"")){
-//if ($this->object_title=="yw_mycity") {
 $lastcondition=gg("yw_mycity.lastcondition");
 $conditioneng=gg("yw_mycity.condition");
-if ($lastcondition<>$conditioneng){
-$condition=$conditioneng; 
-if ($conditioneng=="overcast") {$condition="ясно";}
-if ($conditioneng=="cloudy-and-light-rain") {$condition="пасмурно и небольшой дождь";}
-if ($conditioneng=="cloudy-and-rain") {$condition="пасмурно и  дождь";}
-if ($conditioneng=="cloudy") {$condition="облачно";}
-if ($conditioneng=="overcast-and-light-rain") {$condition="моросящий дождь";}
-if ($conditioneng=="overcast-and-light-snow") {$condition="небольшой снег";}
-if ($conditioneng=="partly-cloudy-and-light-rain") {$condition="переменная облачность и небольшой дождь";}
-if ($conditioneng=="partly-cloudy-and-light-snow") {$condition="переменная облачность и небольшой снег";}
-if ($conditioneng=="partly-cloudy-and-rain") {$condition="переменная облачность с дождем";}
-if ($conditioneng=="partly-cloudy-and-snow") {$condition="переменная облачность со снегом";}
-if ($conditioneng=="partly-cloudy") {$condition="переменная облачность";}
-sg("yw_mycity.lastcondition",$conditioneng) ;
-sg("yw_mycity.lastconditionrus",$condition) ; 
-say(" На улице ".$condition,2);}}
+$condition1eng=gg("yw_mycity.forecast_0_daycondition");  
+$condition2eng=gg("yw_mycity.forecast_1_daycondition ");  
+$condition3eng=gg("yw_mycity.forecast_2_daycondition ");    
+
+  if ($lastcondition<>$conditioneng){
+$condition=conditionrus($conditioneng);
+$condition1=conditionrus($condition1eng);    
+$condition2=conditionrus($condition2eng);        
+$condition3=conditionrus($condition3eng);        
+sg("yw_mycity.lastcondition",$conditioneng);
+sg("yw_mycity.lastconditionrus",$condition); 
+    
+sg("yw_mycity.lastcondition1rus",$condition1) ;     
+sg("yw_mycity.lastcondition2rus",$condition2) ;     
+sg("yw_mycity.lastcondition3rus",$condition3) ;         
+
+    
+    say(" На улице ".$condition,2);
+}
+}
 }
