fe16e99049d295671b7f8db87e61db8fdd733e16
directman66 <sannikovdi@yandex.ru>
2018-06-08 17:34:21 +0500

    Update yandexweather.class.php

--- modules/yandexweather/yandexweather.class.php
+++ modules/yandexweather/yandexweather.class.php
@@ -1505,9 +1505,9 @@ addClassObject('YandexWeather',$objmycity);
 		 
 $sql='SELECT * FROM "methods" where OBJECT_ID=(SELECT id   FROM "objects"  WHERE TITLE ="yw_mycity")';
  
-//$property=SQLSelectOne($sql);
-//$property['CODE']=$Changetemp; //   
-//SQLUpdate('methods',$property);} 
+$property=SQLSelectOne($sql);
+$property['CODE']=$Changetemp; //   
+SQLUpdate('methods',$property); 
 	
 		
 		
