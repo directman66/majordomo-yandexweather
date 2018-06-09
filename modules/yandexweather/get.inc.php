12cfeb6544c9db5a5419fc502ea4fcc3ba6e95fc
directman66 <sannikovdi@yandex.ru>
2018-06-09 13:59:13 +0500

    Update get.inc.php

--- modules/yandexweather/get.inc.php
+++ modules/yandexweather/get.inc.php
@@ -20,7 +20,7 @@ foreach ($properties as $did)
 {
  
 $didr= $did['ID'];
-echo "<br>идем по циклу $didr<br>"; 
+//echo "<br>идем по циклу $didr<br>"; 
 sg('test.vm', $didr);
 $opts = array(
   'http'=>array(
@@ -71,14 +71,13 @@ if ($objn<>"") {
 addClassObject('YandexWeather',$objn);
 $new=1;
 } 
-	
 
-//sg( $objn.'.json',$otvet);
+///////////////////////////
 sg( 'test.vm',$otvet);
 $src=$data['info'];
 sg( $objn.'.now',gg('sysdate').' '.gg('timenow')); 
-echo time();
-echo "<br>"; 
+//echo time();
+//echo "<br>"; 
 	
 foreach ($src as $key=> $value ) { 
 if (is_array($value)) {
@@ -86,18 +85,18 @@ foreach ($value as $key2=> $value2 ) {
 	
 //if (gg($objn.'.'.$key.'_'.$key2)<>$value2) 
 sg( $objn.'.'.$key.'_'.$key2,$value2); 
- echo $objn.'.'.$key.'_'.$key2.":".$value2;
+// echo $objn.'.'.$key.'_'.$key2.":".$value2;
 		     }
 }	
 else	
 {
 //if (gg($objn.'.'.$key.'_'.$key)<>$value)
 	sg( $objn.'.'.$key,$value); 
-echo  $objn.'.'.$key.":".$value;  
-echo "<br>"; 
+//echo  $objn.'.'.$key.":".$value;  
+//echo "<br>"; 
 }
 }     
-
+////////////////////////////////
 //////////////geo_object
 $src=$data['geo_object'];
 foreach ($src as $key=> $value ) {
@@ -105,99 +104,58 @@ if (is_array($value)) {
 foreach ($value as $key2=> $value2 ) {
 //if (gg($objn.'.'.$key.'_'.$key2)!=$value2) 	
 	sg( $objn.'.'.$key.'_'.$key2,$value2); 
-echo $objn.'.'.$key.'_'.$key2.":".$value2;  
- echo "<br>";
+//echo $objn.'.'.$key.'_'.$key2.":".$value2;  
+/// echo "<br>";
 }
 }	
 else	
 {
 //if (gg($objn.'.'.$key.'_'.$key)!=$value)
 	sg( $objn.'.'.$key,$value); 
-	echo $objn.'.'.$key.":".$value;  
+///	echo $objn.'.'.$key.":".$value;  
 }     
 }	
 	
+////////////////////////////////	
+/////fact
 ///////////////////////////////////////////////////	
 $src=$data['fact'];
 	foreach ($src as $key=> $value ) { sg( $objn.'.'.$key,$value); }
-		 
-	
+///////////////////////////////////////////////////	///////////////////////////////////////////////////	
+///////////////////////////////////////////////////	
+///////////////////////////////////////////////////	
+///forecast
+//////////////
 	$fobjn= $objn;
 	$src=$data['forecasts'][0]['parts'];
 foreach ($data['forecasts'] as $day=> $value ) 
 { 
 foreach ($data['forecasts'][$day]['parts'] as $key=> $value ) {    
 if  ($day<=$forecast_day)
-{	
-if (gg( $fobjn.'.'."forecast_".$day."_".$key.'_temp_avg')<>$data['forecasts'][$day]['parts'][$key]['temp_avg']);
+{
+
+
 sg( $fobjn.'.'."forecast_".$day."_".$key.'_temp_avg',$data['forecasts'][$day]['parts'][$key]['temp_avg']);
-echo  $fobjn.'.'."forecast_".$day."_".$key.'_temp_avg'.":".$data['forecasts'][$day]['parts'][$key]['temp_avg'];
-echo "<br>"; 
-
-if (gg( $fobjn.'.'."forecast_".$day."_".$key.'wind_speed')<>$data['forecasts'][$day]['parts'][$key]['wind_speed']);				
 sg( $fobjn.'.'."forecast_".$day."_".$key.'_wind_speed',$data['forecasts'][$day]['parts'][$key]['wind_speed']);
-echo  $fobjn.'.'."forecast_".$day."_".$key.'_wind_speed'.":".$data['forecasts'][$day]['parts'][$key]['wind_speed'];
-echo "<br>";
-if (gg( $fobjn.'.'."forecast_".$day."_".$key.'_wind_gust')<>$data['forecasts'][$day]['parts'][$key]['_wind_gust']);				
 sg( $fobjn.'.'."forecast_".$day."_".$key.'_wind_gust',$data['forecasts'][$day]['parts'][$key]['wind_gust']);
-
-if (gg( $fobjn.'.'."forecast_".$day."_".$key.'wind_dir')<>$data['forecasts'][$day]['parts'][$key]['wind_dir']);								
 sg( $fobjn.'.'."forecast_".$day."_".$key.'_wind_dir',$data['forecasts'][$day]['parts'][$key]['wind_dir']);
-			
-if (gg( $fobjn.'.'."forecast_".$day."_".$key.'_pressure_mm')<>$data['forecasts'][$day]['parts'][$key]['_pressure_mm']);				
 sg( $fobjn.'.'."forecast_".$day."_".$key.'_pressure_mm',$data['forecasts'][$day]['parts'][$key]['pressure_mm']);
-
-if (gg( $fobjn.'.'."forecast_".$day."_".$key.'pressure_pa')<>$data['forecasts'][$day]['parts'][$key]['pressure_pa']);								
 sg( $fobjn.'.'."forecast_".$day."_".$key.'_pressure_pa',$data['forecasts'][$day]['parts'][$key]['pressure_pa']);
-
-if (gg( $fobjn.'.'."forecast_".$day."_".$key.'_humidity')<>$data['forecasts'][$day]['parts'][$key]['_humidity']);								
 sg( $fobjn.'.'."forecast_".$day."_".$key.'_humidity',$data['forecasts'][$day]['parts'][$key]['humidity']);
-
-if (gg( $fobjn.'.'."forecast_".$day."_".$key.'condition')<>$data['forecasts'][$day]['parts'][$key]['condition']);								
 sg( $fobjn.'.'."forecast_".$day."_".$key.'condition',$data['forecasts'][$day]['parts'][$key]['condition']);
-
-if (gg( $fobjn.'.'."forecast_".$day."_".$key.'daytime')<>$data['forecasts'][$day]['parts'][$key]['daytime']);								
 sg( $fobjn.'.'."forecast_".$day."_".$key.'daytime',$data['forecasts'][$day]['parts'][$key]['daytime']); 
+
+
+	
 }
 }
 }
+//////////////////////////////
+
+
+
+
 	
-//mycity	
-	
-	
-	
-$objmycity='yw_mycity';
-	
-//проверяем, нужен ли новый объект	
-//sql="select * from objects where class_id = (select id from classes where title = 'YandexWeather') and objects.TITLE='".$objmycity."'"	;
-//if (empty(SQLSelectOne(sql)['TITLE']))
-//    {
-addClassObject('YandexWeather',$objmycity);	
-    $new=1;
-//    } 	
-	
-$mycity1=SQLSelectOne("SELECT ID FROM `yaweather_cities` where `mycity`=1 ");
-$mycity=$mycity1['ID'];	
-sg($objmycity.'.cityID', $mycity);
- echo $objmycity.'.cityID'.":". $mycity;
-echo "<br>"; 
-echo "мой город ".$mycity;
-echo "<br>"; 
- 
-	
-if ($mycity==$cityid){
-echo "это мой город ".$mycity;
-echo "<br>"; 
-$objprops=get_props1($fobjn);
-foreach ($objprops as $value)
-{ 
- sg($objmycity.'.'.$value,gg($fobjn.".".$value));
-echo $objmycity.'.'.$value.":".gg($fobjn.".".$value); 
-echo "<br>";  
-}
-}
-	
-echo "конец функции получения данных ".date();	
 }
 
 
