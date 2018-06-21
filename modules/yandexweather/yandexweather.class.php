2e6a34bdbf10a3d5681c16a7c6cd3612f7db9c55
directman66 <sannikovdi@yandex.ru>
2018-06-21 18:23:03 +0500

    Update yandexweather.class.php

--- modules/yandexweather/yandexweather.class.php
+++ modules/yandexweather/yandexweather.class.php
@@ -818,7 +818,7 @@ if (($ee=="1") && ($params[\'NEW_VALUE\']!=$lastcondition)) {
 }
 sg("yw_mycity.lastcondition",$this->getProperty("condition"));
 //$cmd_rec = SQLSelectOne("update yaweather_config set value=\'".$this->getProperty("condition")."\' where parametr=\'LASTCONDITION\'");		   	   	   	
-$cmd_rec = SQLSelect("update yaweather_config set value=\'".$params[\'NEW_VALUE\']."\' where parametr=\'LASTCONDITION\'");	
+$cmd_rec = SQLexec("update yaweather_config set value=\'".$params[\'NEW_VALUE\']."\' where parametr=\'LASTCONDITION\'");	
 
 ';
 	 
