1b137a9727a01e78c1b5aa687e0330857915857c
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2019-01-21 11:09:54 +0500

    Update yandexweather.class.php

--- modules/yandexweather/yandexweather.class.php
+++ modules/yandexweather/yandexweather.class.php
@@ -1485,15 +1485,50 @@ return $spl[0] ;
 } 
 
 function getconditionrus($conditioneng){
-  require(DIR_MODULES.$this->name.'/ywext.inc.php');
+//  require(DIR_MODULES.$this->name.'/ywext.inc.php');
 //require(DIR_MODULES.$this->name.'/ywext.php');
 
-$condition=getconditionrusincl($conditioneng);
+$condition=$this->getconditionrusincl($conditioneng);
 return $condition;
 //}
 
 
-}}
+}
+
+
+function getconditionrusincl($conditioneng){
+$condition=$conditioneng;
+if ($conditioneng=="overcast") {$condition="пасмурно";}
+if ($conditioneng=="clear") {$condition="ясно";}	
+if ($conditioneng=="cloudy-and-light-rain") {$condition="пасмурно и небольшой дождь";}
+if ($conditioneng=="cloudy-and-rain") {$condition="пасмурно и  дождь";}
+if ($conditioneng=="cloudy") {$condition="облачно";}
+if ($conditioneng=="overcast-and-light-rain") {$condition="моросящий дождь";}
+if ($conditioneng=="overcast-and-light-snow") {$condition="небольшой снег";}
+if ($conditioneng=="partly-cloudy-and-light-rain") {$condition="переменная облачность и небольшой дождь";}
+if ($conditioneng=="partly-cloudy-and-light-snow") {$condition="переменная облачность и небольшой снег";}
+if ($conditioneng=="partly-cloudy-and-rain") {$condition="переменная облачность с дождем";}
+if ($conditioneng=="partly-cloudy-and-snow") {$condition="переменная облачность со снегом";}
+if ($conditioneng=="partly-cloudy") {$condition="переменная облачность";}
+if ($conditioneng=="overcast-and-rain") {$condition="ливень";}	
+if ($conditioneng=="overcast-thunderstorms-with-rain") {$condition="гроза";}	
+if ($conditioneng=="cloudy-and-snow") {$condition="облачно, легкий снегопад";}	
+if ($conditioneng=="overcast-and-snow") {$condition="небольшой снег";}	
+if ($conditioneng=="cloudy-and-light-snow") {$condition="небольшой снегопад";}	
+if ($conditioneng=="cloudy-and-snow") {$condition="идет снег";}		
+if ($conditioneng=="overcast-and-wet-snow") {$condition="пасмурно и мокрый снег";}		
+	
+	
+	
+	
+	
+//sg("yw_mycity.lastcondition",$conditioneng) ;
+//sg("yw_mycity.lastconditionrus",$condition) ; 
+return $condition;
+}
+
+
+}
 /*
 *
 * TW9kdWxlIGNyZWF0ZWQgQXByIDA0LCAyMDE2IHVzaW5nIFNlcmdlIEouIHdpemFyZCAoQWN0aXZlVW5pdCBJbmMgd3d3LmFjdGl2ZXVuaXQuY29tKQ==
