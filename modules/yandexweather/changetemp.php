23882c3372c913ddc3fa49a39e6faa4875ce6c9a
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2019-02-26 14:01:06 +0500

    Update changetemp.php

--- modules/yandexweather/changetemp.php
+++ modules/yandexweather/changetemp.php
@@ -4,7 +4,7 @@ $curt=gg($par);
 $period="-5 hour";
 $period3="-3 hour";
 $prevt=getHistoryAvg($par, strtotime($period));
-echo $prevt.":".$curt ;
+//echo $prevt.":".$curt ;
 if ($prevt>$curt) { sg("yw_mycity.trandtemp","down");sg("yw_mycity.trandtempfa","fa-arrow-circle-down");}
 else if ($prevt=$curt) { sg("yw_mycity.trandtemp","=");sg("yw_mycity.trandtempfa","fa-pause-circle");}
 else if ($prevt<$curt) { sg("yw_mycity.trandtemp","up");sg("yw_mycity.trandtempfa","fa-arrow-circle-up");}
@@ -12,21 +12,21 @@ sg("yw_mycity.trandtemp-3",getHistoryAvg($par, strtotime($period3)) );
 $par="yw_mycity.pressure_mm";
 $curt=gg($par);
 $prevt=getHistoryAvg($par, strtotime($period));
-echo $prevt.":".$curt ;
+//echo $prevt.":".$curt ;
 if ($prevt>$curt) { sg("yw_mycity.trandpres","down");sg("yw_mycity.trandpresfa","fa-arrow-circle-down");}
 else if ($prevt=$curt) { sg("yw_mycity.trandpres","=");sg("yw_mycity.trandpresfa","fa-pause-circle");}
 else if ($prevt<$curt) { sg("yw_mycity.trandpres","up");sg("yw_mycity.trandpresfa","fa-arrow-circle-up");}
 $par="yw_mycity.humidity";
 $curt=gg($par);
 $prevt=getHistoryAvg($par, strtotime($period));
-echo $prevt.":".$curt ;
+//echo $prevt.":".$curt ;
 if ($prevt>$curt) { sg("yw_mycity.trandhum","down");sg("yw_mycity.trandhumfa","fa-arrow-circle-down");}
 else if ($prevt=$curt) { sg("yw_mycity.trandhum","=");sg("yw_mycity.trandhumfa","fa-pause-circle");}
 else if ($prevt<$curt) { sg("yw_mycity.trandhum","up");sg("yw_mycity.trandhumfa","fa-arrow-circle-up");}
 $par="yw_mycity.wind_speed";
 $curt=gg($par);
 $prevt=getHistoryAvg($par, strtotime($period));
-echo $prevt.":".$curt ;
+//echo $prevt.":".$curt ;
 if ($prevt>$curt) { sg("yw_mycity.trandwind_speed","down");}
 else if ($prevt=$curt) { sg("yw_mycity.trandwind_speed","=");}
 else if ($prevt<$curt) { sg("yw_mycity.trandwind_speed","up");}
